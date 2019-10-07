b73ef620be6b92d061771c5c7c039d0f9f6e3ad8
kose-uk45 <kosebilaliuk45@gmail.com>
2019-10-07 13:27:04 +0200

    Revert "ft(api v2 setup):setup postgresql database" change commit type to chore instead of feature This reverts commit df719311fa978e2acb18bdc39032fb593fbe3780.

--- server/api/v2/controllers/doc.js
+++
@@ -1,6 +0,0 @@
-// controller redirects user to postman doc link, the /api/v1/doc/anytext
-const docs = async (req, res) => {
-  await res.status(302).redirect('https://documenter.getpostman.com/view/9023297/SVtR3WJU');
-};
-
-export default { docs };
--- server/api/v2/routes/doc.js
+++
@@ -1,8 +0,0 @@
-// api documentation route
-import express from 'express';
-import docController from '../controllers/doc';
-
-const router = express.Router();
-router.get('', docController.docs);
-
-export default { router };
