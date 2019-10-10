a75a5512945b2fafdce1d75c6853cdbcbcff61b0
kose-uk45 <kosebilaliuk45@gmail.com>
2019-10-10 18:38:53 +0200

    ch(realease api v2):release app to heroku production

--- server/api/v2/models/dbConfig.js
+++ server/api/v2/models/dbConfig.js
@@ -5,7 +5,7 @@ dotenv.config();
 // heroku style connection string
 // const connectionString = 'postgresql://dbuser:secretpassword@dsomehost:port/mydb'
 const pool = new Pool({
-  connectionString: process.env.DATABASE_URL,
+  connectionString: process.env.DB_Connection_String,
 });
 
 export default pool;
